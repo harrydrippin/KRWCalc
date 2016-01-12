85f7634321b14f880782e9fd71ae385115e1a97f
ScoutLee <dlehdgud2380@icloud.com>
2016-01-13 02:01:16 +0900

    activity_main.xml 수정 #2

--- app/src/main/java/com/kmucs/krwcalc/main/MainActivity.java
+++ app/src/main/java/com/kmucs/krwcalc/main/MainActivity.java
@@ -2,8 +2,7 @@ package com.kmucs.krwcalc.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 import com.kmucs.krwcalc.R;
 
@@ -13,6 +12,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.dotline_1).setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        findViewById(R.id.dotline_2).setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
 }
