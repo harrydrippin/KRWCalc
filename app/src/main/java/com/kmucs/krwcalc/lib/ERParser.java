dcd8e815b22bba66b8186278fbc261c05832d61f
Seunghwan Hong <hj332921@naver.com>
2016-01-14 00:13:25 +0900

    계산 라이브러리 완성, SplashActivity 추가

--- app/src/main/java/com/kmucs/krwcalc/lib/ERParser.java
+++ app/src/main/java/com/kmucs/krwcalc/lib/ERParser.java
@@ -4,5 +4,7 @@ package com.kmucs.krwcalc.lib;
  * Created by harryhong on 16. 1. 13..
  */
 public class ERParser {
-    public void 
+    public float getExchangeRate() {
+        return 1096.37f;
+    }
 }
