defa6d360fd89c903d068aba315f24fb647812ae
Seunghwan Hong <hj332921@naver.com>
2016-01-14 01:30:29 +0900

    SplashActivity 처리 완료

--- app/src/main/java/com/kmucs/krwcalc/main/SplashActivity.java
+++ app/src/main/java/com/kmucs/krwcalc/main/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.kmucs.krwcalc.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import com.kmucs.krwcalc.R;
 
 /**
  * Created by harryhong on 16. 1. 14..
@@ -11,6 +15,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+        }, 1500); // Setting time for waiting
     }
 
 }
