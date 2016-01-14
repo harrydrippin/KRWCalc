6a806343967a8b803e37f08f4aa2238088eb4286
Seunghwan Hong <hj332921@naver.com>
2016-01-14 11:28:22 +0900

    ListAdapter 틀 구현 완료

--- app/src/main/java/com/kmucs/krwcalc/main/MainActivity.java
+++ app/src/main/java/com/kmucs/krwcalc/main/MainActivity.java
@@ -3,10 +3,22 @@ package com.kmucs.krwcalc.main;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.kmucs.krwcalc.R;
+import com.kmucs.krwcalc.lib.Card;
+import com.kmucs.krwcalc.lib.CardCompany;
+import com.kmucs.krwcalc.lib.NetCompany;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private MainListAdapter mAdapter;
+
+    // mAdapter.notifyDataSetChanged();
+    // 리스트 새로고침 역할
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.dotline_1).setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         findViewById(R.id.dotline_2).setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        mListView = (ListView)findViewById(R.id.listview_cardlist);
+
+        mAdapter = new MainListAdapter(this);
+        mListView.setAdapter(mAdapter);
+
+        Card card = new Card();
+        card.setName("카드 이름");
+        card.setCardCompany(CardCompany.HANA);
+
+        mAdapter.addItem(card);
+        mAdapter.addItem(card);
+        mAdapter.addItem(card);
+        mAdapter.addItem(card);
+        mAdapter.addItem(card);
+        mAdapter.addItem(card);
+        mAdapter.addItem(card);
+        mAdapter.addItem(card);
+
     }
 
 }
---
+++ app/src/main/java/com/kmucs/krwcalc/main/MainListAdapter.java
@@ -0,0 +1,80 @@
+package com.kmucs.krwcalc.main;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kmucs.krwcalc.R;
+import com.kmucs.krwcalc.lib.Card;
+
+import java.util.ArrayList;
+
+/**
+ * Created by harryhong on 16. 1. 14..
+ */
+public class MainListAdapter extends BaseAdapter {
+    public Context mContext = null;
+    public ArrayList<Card> mCardData = new ArrayList<Card>();
+
+    public MainListAdapter(Context mContext) {
+        super();
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return mCardData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mCardData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder mViewHolder;
+        if(convertView == null) {
+            mViewHolder = new ViewHolder();
+
+            LayoutInflater mInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.view_mainlist, null);
+
+            mViewHolder.mImageView = (ImageView)convertView.findViewById(R.id.img_cardimg);
+            mViewHolder.mCardName = (TextView)convertView.findViewById(R.id.txt_cardname);
+            mViewHolder.mCardPay = (TextView)convertView.findViewById(R.id.text_pay);
+
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        Card mCard = mCardData.get(position);
+        mViewHolder.mImageView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.card_kookmin_line, null));
+        mViewHolder.mCardName.setText("국민대학교 학생증 체크카드");
+        mViewHolder.mCardPay.setText("1,000");
+
+        return convertView;
+    }
+
+    public void addItem(Card card) {
+        mCardData.add(card);
+    }
+
+    private class ViewHolder {
+        public ImageView mImageView;
+        public TextView mCardName;
+        public TextView mCardPay;
+
+    }
+}
